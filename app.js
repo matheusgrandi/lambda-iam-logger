8d0011770c0671a249447fe4e6fb7175d16ee5bc
matheusgrandi <matheus@matheusgrandi.com.br>
2022-09-11 15:57:50 -0300

    :tada: aws lambda function initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const API_URL = 'http://api.com.br';
+
+export const handler = async (event, context) => {
+  const payload = Buffer(event.awslogs.data, 'base64');
+  const data = (await zlib.gunzipAsync(payload)).toString('utf8');
+  const jsonData = JSON.parse;
+
+  performWork(jsonData);
+
+  try {
+    const result = await performWork(data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      message: ('There was an uncaught error', error),
+    };
+  }
+};
+
+async function* performWork(data) {
+  axios.post(API_URL, data);
+}
